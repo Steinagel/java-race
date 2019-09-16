ac7d760c96a794782c822f966f69a6635ffefe2a
Steinagel <williansteinagel@gmail.com>
2019-09-16 01:54:03 -0300

    Initializes Race operators
    	Adds
    		in_out_put.input:
    			Input_Car.java
    			Input_Pilot.java
    			Input_Road.java
    		in_out_put.output:
    			Output_End.java
    			Output.Start.java
    
    	OBS: *Just initialized files*
    
    Signed-off-by: Steinagel <williansteinagel@gmail.com>

---
+++ src/in_out_put/output/Output_Start.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in_out_put.output;
+
+/**
+ *
+ * @author will_
+ */
+public class Output_Start {
+    
+}
---
+++ src/race_event/Start_Race.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package race_event;
+
+/**
+ *
+ * @author will_
+ */
+public class Start_Race {
+    
+}
